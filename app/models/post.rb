fc4dfdc3a1ed7137948b74c7d23fb96ddef6fb06
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-28 17:45:13 +0300

    Add active storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,6 @@
 class Post < ApplicationRecord
     has_one_attached :image
-
-    validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
-    validates :caption, presence: true
+    validates :caption, :image, presence: true
 
     def imageUrl   
       Rails.application.routes.url_helpers.url_for(image) if image.attached?
